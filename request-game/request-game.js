5b1676f0257f83c4eb760d67ced0a2ecd5e8eb14
grimza99 <gbtmxlf@naver.com>
2025-12-03 23:51:19 +0900

    feat: 요청 게임 폼으로 관련 파일 및 ID 변경

--- request-game/request-game.js
+++ request-game/request-game.js
@@ -1,11 +1,11 @@
-export default function showReplyGameForm() {
-  const modalRoot = document.getElementById("reply-game-form-modal");
+export default function showRequestGameForm() {
+  const modalRoot = document.getElementById("request_game_form_modal");
   if (!modalRoot) return;
 
   modalRoot.innerHTML = `
-        <div id="reply-game-modal-overlay">
-          <div id="reply-game-modal-content">
-            <button class="close-button" id="close_reply_game_modal">x</button>
+        <div id="request_game_modal_overlay">
+          <div id="request_game_modal_content">
+            <button  id="close_request_game_modal">x</button>
             <div id="game-detail-container">   
               <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScG4QDNYfDTwq35x7X_PTrdQfNGpNZu7QXy030IaWUtH2CeTQ/viewform?embedded=true" width="640" height="700" frameborder="0">로드 중…</iframe>
             </div>
@@ -13,7 +13,7 @@ export default function showReplyGameForm() {
         </div>
   `;
 
-  const closeBtn = document.getElementById("close_reply_game_modal");
+  const closeBtn = document.getElementById("close_request_game_modal");
   if (closeBtn) {
     closeBtn.addEventListener("click", () => {
       hideReplyGameForm();
@@ -22,7 +22,7 @@ export default function showReplyGameForm() {
 }
 
 export function hideReplyGameForm() {
-  const modalRoot = document.getElementById("reply-game-form-modal");
+  const modalRoot = document.getElementById("request_game_form_modal");
   if (modalRoot) {
     modalRoot.innerHTML = "";
   }
