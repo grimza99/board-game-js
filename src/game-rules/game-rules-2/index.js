9b03ceccc7e2bef6e6adb889e00e1713625486fb
grimza99 <gbtmxlf@naver.com>
2025-12-09 19:17:03 +0900

    feat: 25 글룸헤이븐 게임 추가 및 규칙 정의

--- src/game-rules/game-rules-2/index.js
+++ src/game-rules/game-rules-2/index.js
@@ -1,4 +1,5 @@
-export { DECRYPTO } from "./21-decrypto.js";
-export { CENTURY_SPICE_ROAD } from "./22-Century-Spice-Road.js";
-export { RESISTANCE_AVALON } from "./23-Resistance-Avalon.js";
-export { SPLENDOR_DUEL } from "./24-splendor-duel.js";
+export { DECRYPTO } from './21-decrypto.js';
+export { CENTURY_SPICE_ROAD } from './22-Century-Spice-Road.js';
+export { RESISTANCE_AVALON } from './23-Resistance-Avalon.js';
+export { SPLENDOR_DUEL } from './24-splendor-duel.js';
+export { GLOOM_HAVEN } from './25-Gloomhaven.js';
