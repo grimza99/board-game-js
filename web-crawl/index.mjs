3e55f1983e1b3551bbd5fd08272ad5f37fc4db95
grimza99 <gbtmxlf@naver.com>
2025-12-17 17:07:41 +0900

    feat: CLI에서 대화형으로 게임 추가 기능 구현

--- web-crawl/index.mjs
+++ web-crawl/index.mjs
@@ -1,25 +1,56 @@
 import { crawlAndSave } from './crawl-save.mjs';
 import { saveNormalizedJson } from './normalize.mjs';
+import readline from 'readline';
 
-const games = [
-  {
-    gameId: 1,
-    gameName: 'Carcassonne',
-    url: 'https://www.ultraboardgames.com/carcassonne/game-rules.php',
-  },
-];
+function ask(rl, question) {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer.trim());
+    });
+  });
+}
 
 async function runProcess() {
-  for (const game of games) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  console.log('\nStart BoardWiki CLI\n');
+  try {
+    const gameId = await ask(rl, 'Game ID: ');
+    const gameName = await ask(rl, 'Game Name: ');
+    const url = await ask(rl, 'Game URL: ');
+
+    if (!gameId || !gameName || !url) {
+      console.error('\n❌ 모든 값을 입력해야 합니다.');
+      rl.close();
+      return;
+    }
+
+    const game = {
+      gameId: Number(gameId),
+      gameName,
+      url,
+    };
+
     console.log(
       `\n=============== [PROCESS START]: ${game.gameName} ===============`
     );
-
     await crawlAndSave(game);
     saveNormalizedJson(game);
-
     console.log(`\n=============== [DONE]: ${game.gameName} ===============`);
+
+    const next = await ask(rl, '게임을 더 추가하나요? (y/n): ');
+
+    if (next.toLowerCase() === 'y') {
+      runProcess();
+    } else {
+      rl.close();
+    }
+  } catch (e) {
+    console.log('\n[ERROR]', e.message);
+    console.log('\ncli가 종료됩니다.');
+    rl.close();
   }
 }
-
-runProcess().catch(console.error);
+runProcess();
