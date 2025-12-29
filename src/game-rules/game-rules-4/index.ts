70efcc71ae20eb1919c0d9d51198dec75b909c4b
grimza99 <gbtmxlf@naver.com>
2025-12-29 17:26:41 +0900

    feat: 75~78 게임 추가

--- src/game-rules/game-rules-4/index.ts
+++ src/game-rules/game-rules-4/index.ts
@@ -2,13 +2,17 @@ export { PATCHWORK } from './61-Patchwork.ts';
 export { A_FEAST_FOR_ODIN } from './62-AFeastForOdin.ts';
 export { BLOOD_RAGE } from './63-BloodRage.ts';
 export { TRANJAN } from './64-Trajan.ts';
-export {PANDEMIC_LEGACY_SEASON_1 }from './65-pandemic-legacy-season-1.ts'
-export {DUNE_IMPERIUM }from './66-dune-imperium.ts'
-export {ARKHAM_HORROR_THE_CARD_GAME }from './67-arkham-horror-the-card-game.ts'
-export {NEMESIS }from './68-nemesis.ts'
-export {ECLIPSE }from './69-eclipse.ts'
-export {MAGE_KNIGHT_BOARD_GAME }from './70-mage-knight-board-game.ts'
-export {ANACHRONY }from './71-anachrony.ts'
-export {TZOLK_IN_THE_MAYAN_CALENDAR }from './72-tzolk-in-the-mayan-calendar.ts'
-export {ON_MARS }from './73-on-mars.ts'
-export {JAIPUR }from './74-jaipur.ts'
+export { PANDEMIC_LEGACY_SEASON_1 } from './65-pandemic-legacy-season-1.ts';
+export { DUNE_IMPERIUM } from './66-dune-imperium.ts';
+export { ARKHAM_HORROR_THE_CARD_GAME } from './67-arkham-horror-the-card-game.ts';
+export { NEMESIS } from './68-nemesis.ts';
+export { ECLIPSE } from './69-eclipse.ts';
+export { MAGE_KNIGHT_BOARD_GAME } from './70-mage-knight-board-game.ts';
+export { ANACHRONY } from './71-anachrony.ts';
+export { TZOLK_IN_THE_MAYAN_CALENDAR } from './72-tzolk-in-the-mayan-calendar.ts';
+export { ON_MARS } from './73-on-mars.ts';
+export { JAIPUR } from './74-jaipur.ts';
+export { CAVERNA_THE_CAVE_FARMERS } from './75-caverna-the-cave-farmers.ts';
+export { PAX_PAMIR_SECOND_EDITION } from './76-pax-pamir-second-edition.ts';
+export { THE_GALLERIST } from './77-the-gallerist.ts';
+export { UNDERWATER_CITIES } from './78-underwater-cities.ts';
